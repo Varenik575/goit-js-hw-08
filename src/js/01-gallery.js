4d9bedbd70e40360f66638292278a390893d0312
Varenik575 <invisibleredhead5@gmail.com>
2023-07-29 18:41:59 +0300

    second commit (issues)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageCardsMarkup = createImageCards(galleryItems);
+const imageCardContainer = document.querySelector('.gallery');
+insertMarkup(imageCardsMarkup, imageCardContainer);
+
+imageCardContainer.addEventListener('click', onImageClick);
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+    captionsData: `alt`,
+    captionDelay: 250,
+  });
+
+
+function createImageCards(images) {
+
+    return images.map(({ preview, original, description }) => 
+        
+      `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+
+    ).join('\n\n');
+  
+};
+
+function insertMarkup(markup, location) {
+
+  location.insertAdjacentHTML('beforeend', markup)
+  
+}
+
+function onImageClick(event) {
+
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+    };
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function({seconds}) {
+    localStorage.setItem("video-current-time", JSON.stringify({seconds}));
+};
+
+const currentTime = localStorage.getItem("video-current-time");
+
+
+player.on('timeupdate', onPlay);
+
+player.setCurrentTime(parseInt(currentTime)).then(function (seconds) {}).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        
+        default:
+            break;
+    }
+});
