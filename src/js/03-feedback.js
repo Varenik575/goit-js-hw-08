f66d9f82c758e77eb9609596453ed2ed009f304d
Varenik575 <invisibleredhead5@gmail.com>
2023-07-31 18:12:11 +0300

    fifth commit (issues)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,26 +1,30 @@
-const { values } = require("@vimeo/player");
+import throttle from "lodash.throttle";
 
 const form = document.querySelector('.feedback-form');
 const submitBtn = document.querySelector('button');
-
-
 const email = form.querySelector('input');
 const message = form.querySelector('textarea');
+const values = JSON.parse(localStorage.getItem('feedback-form-state'));
 
-const currentContent = Array.from(form.elements);
+email.addEventListener('input', throttle(onFieldInput, 500));
+message.addEventListener('input', throttle(onFieldInput, 500));
+form.addEventListener('submit', clearLocalStorage);
 
-form.addEventListener('input', onFormInput);
-form.addEventListener('submit', onFormSubmit);
+FillFields();
 
-
-
-function onFormInput(event) {
-    localStorage.setItem("feedback-form-state", currentContent);
-}
-
-
-function onFormSubmit(event) {
+function onFieldInput(event) {
     event.preventDefault();
+    localStorage.setItem('feedback-form-state', JSON.stringify({ email: email.value, message: message.value }));
 }
 
-console.log(currentContent);
+function FillFields() {
+    if (values) {
+        email.value = values.email;
+        message.value = values.message;
+    };
+}
+
+function clearLocalStorage() {
+    console.log(values);
+    localStorage.removeItem('feedback-form-state');
+}
